a5c8a02dbd2c8cb6d281049cb030c8082523507d
Abe Dolinger <a.d@256hz.com>
2019-08-29 19:07:57 -0700

    started work on categorizing colors on database entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,21 @@
-require 'securerandom'
 Color.destroy_all
+
+def absolute_colors
+  {
+    'red' => [255, 0, 0],
+    'orange' => [255, 127, 0],
+    'yellow' => [255, 255, 0],
+    'green' => [0, 255, 0],
+    'blue' => [0, 0, 255],
+    'purple' => [127, 0, 255],
+    'brown' => [127, 96, 40],
+    'gray' => [127, 127, 127]
+  }
+end
+
+def color_family(hex)
+  r, g, b = hex[0..1].hex, hex[2..3].hex, hex[4..5].hex
+end
 
 100.times do
   color_value = SecureRandom.hex(3)
